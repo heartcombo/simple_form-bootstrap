26009f4843d1f191b6d1003092e736a92ae8bca5
Fabio Yamate <fabio.yamate@plataformatec.com.br>
2011-09-27 01:45:28 -0300

    Add append input type.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -31,8 +31,19 @@ SimpleForm.setup do |config|
     b.use :hint,  :tag => :span, :class => :'help-block'
     b.use :tag => 'div', :class => 'input' do |input|
       input.use :tag => 'div', :class => 'input-prepend' do |prepend|
-        prepend.use :tag => :span, :class => "add-on"
         prepend.use :input
+      end
+      input.use :error, :tag => :span, :class => :'help-inline'
+    end
+  end
+
+  config.wrappers :append, :class => "clearfix", :error_class => :error do |b|
+    b.use :placeholder
+    b.use :label
+    b.use :hint,  :tag => :span, :class => :'help-block'
+    b.use :tag => 'div', :class => 'input' do |input|
+      input.use :tag => 'div', :class => 'input-append' do |append|
+        append.use :input
       end
       input.use :error, :tag => :span, :class => :'help-inline'
     end
