0c2a87d0fe040f6eca9e0e92a78087b196cffd5e
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-06-02 15:36:17 -0300

    Fix error notification box styling
    See https://github.com/plataformatec/simple_form/issues/586.
    Thanks to @tkrotoff

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -104,7 +104,7 @@ SimpleForm.setup do |config|
   config.error_notification_tag = :div
 
   # CSS class to add for error notification helper.
-  config.error_notification_class = 'alert alert-error'
+  # config.error_notification_class = 'alert alert-error'
 
   # ID to add for error notification helper.
   # config.error_notification_id = nil
