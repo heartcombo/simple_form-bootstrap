0a1d496d20bc2f706f399662045a861970954a62
m5o <m5o@gmx.com>
2018-02-26 17:21:38 +0100

    delete old database migrations

--- db/migrate/20110911180155_add_content_type_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddContentTypeToArticle < ActiveRecord::Migration[4.2]
-  def change
-    add_column :articles, :content_type, :string
-  end
-end
--- db/migrate/20110911190941_add_disabled_text_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddDisabledTextToArticle < ActiveRecord::Migration[4.2]
-  def change
-    add_column :articles, :disabled_text, :string
-  end
-end
