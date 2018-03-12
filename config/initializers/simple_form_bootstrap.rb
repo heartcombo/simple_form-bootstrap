50fcff04f987a778177b5f6391663f940fa73fe8
m5o <m5o@gmx.com>
2018-03-12 10:40:33 +0100

    fix minor styling issues

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -382,19 +382,22 @@ end
 
 
 # input group
-# custom component
+#
+# custom component requires input group wrapper
 module SimpleForm
   module Components
     module InputGroups
       module Prepends
         def prepend(wrapper_options = nil)
-          template.content_tag(:div, content_tag(:span, options[:prepend], class: "input-group-text"), class: "input-group-prepend")
+          span_tag = content_tag(:span, options[:prepend], class: "input-group-text")
+          template.content_tag(:div, span_tag, class: "input-group-prepend")
         end
       end
 
       module Appends
         def append(wrapper_options = nil)
-          template.content_tag(:div, content_tag(:span, options[:append], class: "input-group-text"), class: "input-group-append")
+          span_tag = content_tag(:span, options[:append], class: "input-group-text")
+          template.content_tag(:div, span_tag, class: "input-group-append")
         end
       end
     end
