57c94b64f16c38f386c76b942ecc779d53e87561
m5o <m5o@gmx.com>
2018-03-29 13:45:21 +0200

    add styling section to documentation

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -18,7 +18,10 @@ module LayoutHelper
     Kramdown::Document.new(source, options).to_html.html_safe
   end
 
-  def render_source(input, language = :erb)
-    markdown "```#{language.to_s}\n#{input}```"
+  def render_source(input, language = :erb, filename = nil)
+    capture do
+      concat content_tag(:p, "📂 #{filename}", class: "bg-light border small font-weight-bold px-4 py-2 mb-0 mt-4") if filename
+      concat markdown("```#{language.to_s}\n#{input}```")
+    end
   end
 end
