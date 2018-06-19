c1ae7732d27181288cc872c914172ad7bf00dc95
Rafael Mendonça França <rafaelmfranca@gmail.com>
2018-06-19 12:27:33 -0400

    Force table creation

--- db/migrate/20180219221860_create_users.rb
+++ db/migrate/20180219221860_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :users, force: true do |t|
       t.string :name
       t.string :email
       t.string :password
