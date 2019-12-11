40b5928a44034047d649e0eca86910344ba72d91
m5o <m5o@gmx.com>
2019-12-11 17:24:01 +0100

    fix minitest deprecation warning
    * fix minitest deprecation warning
      * DEPRECATED: global use of must_match from test/helpers/error_messages_helper_test.rb:11. Use _(obj).must_match instead. This will fail in Minitest 6.

--- test/helpers/error_messages_helper_test.rb
+++ test/helpers/error_messages_helper_test.rb
@@ -8,11 +8,11 @@ class ErrorMessagesHelperTest < ActionView::TestCase
     end
 
     it "display error messages as html" do
-      error_messages_for(@model).must_match '<div class="alert alert-danger alert-form alert-dismissible">'
-      error_messages_for(@model).must_match '<a href="#" class="close" data-dismiss="alert">×</a>'
-      error_messages_for(@model).must_match '<h6 class="text-uppercase text-reset my-1">'
-      error_messages_for(@model).must_match '<p>There were problems with the'
-      error_messages_for(@model).must_match '<li>Name is too short (minimum is 3 characters)</li>'
+      _(error_messages_for(@model)).must_match '<div class="alert alert-danger alert-form alert-dismissible">'
+      _(error_messages_for(@model)).must_match '<a href="#" class="close" data-dismiss="alert">×</a>'
+      _(error_messages_for(@model)).must_match '<h6 class="text-uppercase text-reset my-1">'
+      _(error_messages_for(@model)).must_match '<p>There were problems with the'
+      _(error_messages_for(@model)).must_match '<li>Name is too short (minimum is 3 characters)</li>'
     end
   end
 end
