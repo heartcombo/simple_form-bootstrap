2f36f06c291e970607752cf627784158ce1ca24a
m5o <m5o@gmx.com>
2020-10-01 11:55:59 +0200

    remove custom uglifier config (#280)
    * remove custom uglifier config
    * fix for failing heroku deployments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,9 +109,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-
-  # custom
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
 end
