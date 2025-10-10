2175f2373f9c50ebb36b4ca3071b2e29ed2259b8
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-10-10 11:58:30 -0300

    Stop running tests in parallel
    This is failing locally and I'm not sure / don't care why for now.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require 'minitest/autorun'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
