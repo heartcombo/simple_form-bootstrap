16edd1715ec544490c1871346f8fce069f256541
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-10-10 13:39:48 -0300

    Rename component module to match expected from file name/structure
    Zeitweirk was complaning:
    
        /home/runner/work/simple_form-bootstrap/simple_form-bootstrap/vendor/bundle/ruby/3.4.0/gems/zeitwerk-2.7.3/lib/zeitwerk/cref.rb:62:in
        'Module#const_get': uninitialized constant Components::InputGroupComponent (NameError)

--- lib/components/input_group_component.rb
+++ lib/components/input_group_component.rb
@@ -1,13 +1,16 @@
 # custom component requires input group wrapper
-module InputGroup
-  def prepend(wrapper_options = nil)
-    template.content_tag(:span, options[:prepend], class: "input-group-text")
-  end
 
-  def append(wrapper_options = nil)
-    template.content_tag(:span, options[:append], class: "input-group-text")
+module Components
+  module InputGroupComponent
+    def prepend(wrapper_options = nil)
+      template.content_tag(:span, options[:prepend], class: "input-group-text")
+    end
+
+    def append(wrapper_options = nil)
+      template.content_tag(:span, options[:append], class: "input-group-text")
+    end
   end
 end
 
 # Register the component in Simple Form.
-SimpleForm.include_component(InputGroup)
+SimpleForm.include_component(Components::InputGroupComponent)
